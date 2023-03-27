4e37fb04cf62fd0feb4f7c68c9552c5ceadc11c9
Bence Molnár <bence.molnar@snapsoft.hu>
2023-03-27 19:26:21 +0200

    add login screen bg animation

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,5 @@
 import ProviderButton from "@/components/auth/ProviderButton";
-import { GhostColumn } from "@/components/ghost/GhostBoard";
+import { GhostBoard } from "@/components/ghost/GhostBoard";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
@@ -21,16 +21,10 @@ export default async function Landing({
 
   return (
     <main className="min-h-[100svh] relative">
-      <section className="max-h-screen grid grid-flow-col p-4 gap-6 overflow-hidden opacity-5 dark:opacity-20">
-        {Array(5)
-          .fill(0)
-          .map((_, i) => (
-            <GhostColumn key={i} index={i} />
-          ))}
-      </section>
+      <GhostBoard />
       <section className="p-4 absolute inset-0 grid place-items-center">
         <div className="w-full max-w-md space-y-4 border-2 border-t-8 rounded-md p-4 shadow-xl bg-white dark:bg-zinc-900">
-          <h3>Tábla</h3>
+          <h1 className="mb-24">Tábla</h1>
           {providers &&
             Object.values(providers).map((provider) => (
               <ProviderButton
--- src/components/ghost/GhostBoard.tsx
+++ src/components/ghost/GhostBoard.tsx
@@ -1,44 +1,194 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
-export function GhostColumn({ index }: { index: number }) {
-  const rand = useRef(mulberry32(index));
-  const titleWidth = useRef(Math.floor(rand.current() * 120 + 120));
+const easeIn = "cubic-bezier(0.4, 0, 1, 1)";
+const easeOut = "cubic-bezier(0, 0, 0.2, 1)";
+const easeInOut = "cubic-bezier(0.4, 0, 0.2, 1)";
+
+function dragBefore(fromSelector: string, toSelector: string) {
+  const from = document.querySelector(fromSelector) as HTMLElement;
+  const to = document.querySelector(toSelector) as HTMLElement;
+  let taskElementsAbove = document.querySelectorAll(
+    `${toSelector}, ${toSelector} ~ li:has(~ #${from.id})`
+  );
+  const tasksAbove = Array.from(taskElementsAbove);
+  tasksAbove.reverse();
+  const fromRect = from.getBoundingClientRect();
+  const toRect = to.getBoundingClientRect();
+  const deltaY = fromRect.top - toRect.top;
+  from.style["zIndex"] = "10";
+  const liftOptions = {
+    delay: 4000,
+    duration: 300,
+    fill: "forwards",
+    easing: easeOut,
+  } as KeyframeAnimationOptions;
+  const liftFrames = [
+    { transform: "translate(0, 0)" },
+    { transform: "translate(10%, 0)" },
+  ] as Keyframe[];
+  const dragOptions = {
+    duration: deltaY * 2.5,
+    fill: "forwards",
+    easing: easeInOut,
+  } as KeyframeAnimationOptions;
+  const dragFrames = [
+    {
+      transform: `translate(10%, ${-deltaY}px)`,
+    },
+  ] as Keyframe[];
+  const staggerOptions = {
+    duration: 600,
+    easing: easeInOut,
+    fill: "forwards",
+  } as KeyframeAnimationOptions;
+  const staggerFrames = [
+    { transform: "translateY(0)" },
+    { transform: `translateY(${fromRect.height}px)` },
+  ];
+  const dropOptions = {
+    duration: 300,
+    fill: "forwards",
+    easing: easeIn,
+  } as KeyframeAnimationOptions;
+  const dropFrames = [
+    { transform: `translate(0, ${-deltaY}px)` },
+  ] as Keyframe[];
+  const liftAnimation = from.animate(liftFrames, liftOptions);
+  liftAnimation.pause();
+  const dragAnimation = from.animate(dragFrames, dragOptions);
+  dragAnimation.pause();
+  const staggerAnimation = tasksAbove.map((task, i) => {
+    const animation = task.animate(staggerFrames, {
+      ...staggerOptions,
+      delay: 300 + i * 150,
+    });
+    animation.pause();
+    return animation;
+  });
+  const dropAnimation = from.animate(dropFrames, dropOptions);
+  dropAnimation.pause();
+  liftAnimation.finished
+    .then(() => {
+      dragAnimation.play();
+      staggerAnimation.forEach((animation) => animation.play());
+      return dragAnimation.finished;
+    })
+    .then(() => {
+      dropAnimation.play();
+      return dropAnimation.finished;
+    })
+    .catch(() => {});
+
+  dropAnimation.addEventListener("finish", () => {
+    liftAnimation.cancel();
+    dragAnimation.cancel();
+    staggerAnimation.forEach((animation) => animation.cancel());
+    dropAnimation.cancel();
+    from.removeAttribute("style");
+    to.parentElement!.insertBefore(from, to);
+  });
+
+  return {
+    play: () => liftAnimation.play(),
+    cancel: () => {
+      liftAnimation.cancel();
+      dragAnimation.cancel();
+      staggerAnimation.forEach((animation) => animation.cancel());
+      dropAnimation.cancel();
+    },
+    animation: dropAnimation,
+    finished: dropAnimation.finished,
+  };
+}
+
+function animateBoard() {
+  const frames: [string, string][] = [
+    ["#col_2 li:nth-child(10)", "#col_2 li:nth-child(4)"],
+    ["#col_0 li:nth-child(9)", "#col_0 li:nth-child(2)"],
+    ["#col_1 li:nth-child(10)", "#col_1 li:nth-child(5)"],
+    ["#col_3 li:nth-child(12)", "#col_3 li:nth-child(9)"],
+    ["#col_4 li:nth-child(6)", "#col_4 li:nth-child(1)"],
+  ];
+  let timeline: ReturnType<typeof dragBefore>[] = [];
+  const play = () => {
+    frames.forEach((frame, index) => {
+      const animation = dragBefore(...frame);
+      timeline.push(animation);
+      if (index > 0) {
+        timeline[index - 1].animation.onfinish = () => animation.play();
+      }
+      if (index === frames.length - 1) {
+        timeline[frames.length - 1].animation.onfinish = () => {
+          timeline = [];
+          play();
+        };
+      }
+    });
+    timeline[0].play();
+  };
+  play();
+  return {
+    cancel: () => {
+      timeline.forEach((animation) => animation.cancel());
+    },
+  };
+}
+
+export function GhostBoard() {
+  useEffect(() => {
+    const animation = animateBoard();
+    return () => animation.cancel();
+  }, []);
 
   return (
-    <section className="flex flex-col">
-      <header className="h-14 pb-3">
-        <h5
-          className="rounded-md bg-zinc-800"
-          style={{ width: titleWidth.current }}
-        >
-          &nbsp;
-        </h5>
-      </header>
-      <ul>
-        {Array(16)
-          .fill(0)
-          .map((_, i) => (
-            <li key={i}>
-              <GhostTask index={(index + i) * i} />
-            </li>
-          ))}
-      </ul>
+    <section className="max-h-screen grid grid-flow-col auto-cols-[minmax(20ch,_30ch)] p-4 gap-6 overflow-hidden opacity-5 dark:opacity-20">
+      {Array(5)
+        .fill(0)
+        .map((_, colIndex) => (
+          <GhostColumn key={colIndex} index={colIndex}>
+            {Array(16)
+              .fill(0)
+              .map((_, rowIndex) => (
+                <li
+                  key={rowIndex}
+                  id={`task_${colIndex}_${rowIndex}`}
+                  className="relative pb-3"
+                >
+                  <GhostTask index={(colIndex + rowIndex) * rowIndex} />
+                </li>
+              ))}
+          </GhostColumn>
+        ))}
+    </section>
+  );
+}
+
+export function GhostColumn({
+  index,
+  children,
+}: {
+  index: number;
+  children?: React.ReactNode;
+}) {
+  return (
+    <section id={`col_${index}`} className="flex flex-col -translate-y-10">
+      <ul>{children}</ul>
     </section>
   );
 }
 
 export function GhostTask({ index }: { index: number }) {
   const rand = useRef(mulberry32(index));
-  const titleWidth = useRef(Math.floor(rand.current() * 120 + 120));
+  const titleWidth = useRef(Math.floor(30 + rand.current() * 70));
   const descriptionLines = useRef(Math.floor(rand.current() * 2 + 1));
 
   return (
-    <div className="mb-3 p-2 rounded-md border-2 border-t-8 space-y-2">
+    <div className="p-2 rounded-md border-2 border-t-8 space-y-2 bg-white dark:bg-zinc-900">
       <h3
         className="rounded-md bg-zinc-800"
-        style={{ width: titleWidth.current }}
+        style={{ width: titleWidth.current + "%" }}
       >
         &nbsp;
       </h3>
