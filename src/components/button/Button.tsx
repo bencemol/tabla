1c08eb146df64b067ca571c56ce9e09e4c04ed35
Bence Molnár <bence.molnar@snapsoft.hu>
2023-03-28 14:15:54 +0200

    remove button hover animation

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -12,7 +12,7 @@ const variants = {
     "bg-white disabled:border-zinc-700 dark:bg-zinc-900 dark:border-white dark:disabled:border-zinc-400",
   danger:
     "bg-white text-red-800 border-red-800 disabled:border-red-700 dark:bg-zinc-900 dark:text-red-300 dark:border-red-300 dark:disabled:border-red-400",
-  flat: "border-transparent bg-transparent hover:!-translate-y-0 active:!translate-y-0 hover:bg-zinc-300 active:bg-zinc-400 disabled:bg-transparent disabled:text-zinc-200 dark:bg-transparent dark:hover:bg-zinc-700 dark:active:bg-zinc-600 dark:disabled:bg-transparent",
+  flat: "border-transparent bg-transparent hover:bg-zinc-300 active:bg-zinc-400 disabled:bg-transparent disabled:text-zinc-200 dark:bg-transparent dark:hover:bg-zinc-700 dark:active:bg-zinc-600 dark:disabled:bg-transparent",
   ghost:
     "bg-transparent border-dashed border-zinc-200 dark:border-zinc-800 hover:border-transparent text-zinc-200 hover:text-inherit focus-visible:focus-within:text-inherit dark:text-zinc-800 hover:!bg-zinc-100 dark:hover:!bg-zinc-800 hover:!-translate-y-0 active:!translate-y-0",
 };
@@ -29,7 +29,7 @@ export default function Button({
   return (
     <button
       type={type}
-      className={`relative border-2 rounded-md transition-colors disabled:cursor-not-allowed hover:-translate-y-0.5 active:translate-y-0.5 ${variants[variant]} ${className}`}
+      className={`relative border-2 rounded-md transition-colors disabled:cursor-not-allowed active:translate-y-0.5 ${variants[variant]} ${className}`}
       style={{
         transitionProperty:
           "transform, color, background-color, border-color, text-decoration-color, fill, stroke",
